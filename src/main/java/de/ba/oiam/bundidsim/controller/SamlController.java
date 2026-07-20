4952c6a18aa16812796469c4382ed29e35144fe7
Tom Schneider <228496304+tomschneider-ba@users.noreply.github.com>
2026-07-20 14:58:01 +0200

    chore: extract skeleton saml request
    to avoid cryptic string in code.
    
    Signed-off-by: Tom Schneider <228496304+tomschneider-ba@users.noreply.github.com>

--- src/main/java/de/ba/oiam/bundidsim/controller/SamlController.java
+++ src/main/java/de/ba/oiam/bundidsim/controller/SamlController.java
@@ -58,10 +58,9 @@ public class SamlController {
     @GetMapping(path = "/saml")
     public String samlRequestReceiverGet(Model model) throws Exception {
 
-        String paramSamlRequest =
-                "PHNhbWxwOkF1dGhuUmVxdWVzdCB4bWxuczpzYW1scD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOnByb3RvY29sIiB4bWxucz0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOmFzc2VydGlvbiIgeG1sbnM6c2FtbD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOmFzc2VydGlvbiIgQXNzZXJ0aW9uQ29uc3VtZXJTZXJ2aWNlVVJMPSJodHRwczovL3NlY3VyZS5kZXYub2NwLndlYmFwcC5pZHN0LmliYWludGVybi5kZS9laWQtaW5mcmEvYXV0aC9yZWFsbXMvQnVuZElELUlkZW50L2Jyb2tlci9idW5kaWQvZW5kcG9pbnQiIEF0dHJpYnV0ZUNvbnN1bWluZ1NlcnZpY2VJbmRleD0iMCIgRGVzdGluYXRpb249Imh0dHBzOi8vc2VjdXJlLmRldi5vY3Aud2ViYXBwLmlkc3QuaWJhaW50ZXJuLmRlL2VpZC1pbmZyYS9pZHBtb2NrL3NhbWwiIEZvcmNlQXV0aG49ImZhbHNlIiBJRD0iSURfNTVhYTFkODYtYmIzZi00YmJkLTkyMDktNWNlZTVkMzMxN2E3IiBJc3N1ZUluc3RhbnQ9IjIwMjMtMTItMTFUMTM6MzM6MjQuODUyWiIgUHJvdG9jb2xCaW5kaW5nPSJ1cm46b2FzaXM6bmFtZXM6dGM6U0FNTDoyLjA6YmluZGluZ3M6SFRUUC1QT1NUIiBWZXJzaW9uPSIyLjAiPjxzYW1sOklzc3Vlcj5odHRwczovL3NlY3VyZS5kZXYub2NwLndlYmFwcC5pZHN0LmliYWludGVybi5kZS9laWQtaW5mcmEvYXV0aC9yZWFsbXMvQnVuZElELUlkZW50PC9zYW1sOklzc3Vlcj48c2FtbHA6RXh0ZW5zaW9ucz48YWtkYjpBdXRoZW50aWNhdGlvblJlcXVlc3QgeG1sbnM6YWtkYj0iaHR0cHM6Ly93d3cuYWtkYi5kZS9yZXF1ZXN0LzIwMTgvMDkiIFZlcnNpb249IjIiPjxha2RiOlJlcXVlc3RlZEF0dHJpYnV0ZXM+PGFrZGI6UmVxdWVzdGVkQXR0cmlidXRlIE5hbWU9InVybjpvaWQ6MS4yLjQwLjAuMTAuMi4xLjEuMjI1NTk5IiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48YWtkYjpSZXF1ZXN0ZWRBdHRyaWJ1dGUgTmFtZT0idXJuOm9pZDoxLjMuNi4xLjQuMS4yNTQ4NC40OTQ0NTAuMTAuMSIgUmVxdWlyZWRBdHRyaWJ1dGU9ImZhbHNlIi8+PGFrZGI6UmVxdWVzdGVkQXR0cmlidXRlIE5hbWU9InVybjpvaWQ6MS4yLjQwLjAuMTAuMi4xLjEuMjYxLjk0IiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48YWtkYjpSZXF1ZXN0ZWRBdHRyaWJ1dGUgTmFtZT0idXJuOm9pZDoyLjUuNC43IiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48YWtkYjpSZXF1ZXN0ZWRBdHRyaWJ1dGUgTmFtZT0idXJuOm9pZDoyLjUuNC4xNiIgUmVxdWlyZWRBdHRyaWJ1dGU9ImZhbHNlIi8+PGFrZGI6UmVxdWVzdGVkQXR0cmlidXRlIE5hbWU9InVybjpvaWQ6MS4zLjYuMS40LjEuMjU0ODQuNDk0NDUwLjIiIFJlcXVpcmVkQXR0cmlidXRlPSJmYWxzZSIvPjxha2RiOlJlcXVlc3RlZEF0dHJpYnV0ZSBOYW1lPSJ1cm46b2lkOjIuNS40LjQiIFJlcXVpcmVkQXR0cmlidXRlPSJmYWxzZSIvPjxha2RiOlJlcXVlc3RlZEF0dHJpYnV0ZSBOYW1lPSJ1cm46b2lkOjEuMy42LjEuNS41LjcuOS4yIiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48YWtkYjpSZXF1ZXN0ZWRBdHRyaWJ1dGUgTmFtZT0idXJuOm9pZDoyLjUuNC4xNyIgUmVxdWlyZWRBdHRyaWJ1dGU9ImZhbHNlIi8+PGFrZGI6UmVxdWVzdGVkQXR0cmlidXRlIE5hbWU9InVybjpvaWQ6MC45LjIzNDIuMTkyMDAzMDAuMTAwLjEuNDAiIFJlcXVpcmVkQXR0cmlidXRlPSJmYWxzZSIvPjxha2RiOlJlcXVlc3RlZEF0dHJpYnV0ZSBOYW1lPSJ1cm46b2lkOjEuMi40MC4wLjEwLjIuMS4xLjU1IiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48YWtkYjpSZXF1ZXN0ZWRBdHRyaWJ1dGUgTmFtZT0idXJuOm9pZDoyLjUuNC40MiIgUmVxdWlyZWRBdHRyaWJ1dGU9ImZhbHNlIi8+PGFrZGI6UmVxdWVzdGVkQXR0cmlidXRlIE5hbWU9InVybjpvaWQ6MS4yLjQwLjAuMTAuMi4xLjEuMjI1NTY2IiBSZXF1aXJlZEF0dHJpYnV0ZT0iZmFsc2UiLz48L2FrZGI6UmVxdWVzdGVkQXR0cmlidXRlcz48L2FrZGI6QXV0aGVudGljYXRpb25SZXF1ZXN0Pjwvc2FtbHA6RXh0ZW5zaW9ucz48c2FtbHA6TmFtZUlEUG9saWN5IEFsbG93Q3JlYXRlPSJmYWxzZSIgRm9ybWF0PSJ1cm46b2FzaXM6bmFtZXM6dGM6U0FNTDoxLjE6bmFtZWlkLWZvcm1hdDp1bnNwZWNpZmllZCIvPjxzYW1scDpSZXF1ZXN0ZWRBdXRobkNvbnRleHQgQ29tcGFyaXNvbj0ibWluaW11bSI+PHNhbWw6QXV0aG5Db250ZXh0Q2xhc3NSZWY+U1RPUkstUUFBLUxldmVsLTM8L3NhbWw6QXV0aG5Db250ZXh0Q2xhc3NSZWY+PC9zYW1scDpSZXF1ZXN0ZWRBdXRobkNvbnRleHQ+PC9zYW1scDpBdXRoblJlcXVlc3Q+";
+        String paramSamlRequest = createSkeletonSamlRequestBase64Encoded();
         String paramRelayState =
-                "lwN0_rRBl5jHaEJt4a0V6IRFOkaR3Krw4K9HQ-73NSM.1gou_vqz1yw.ocp-api-client-public";
+                "lwN0_rRBl5jHaEJt4a0V6IRFOkaR3Krw4K9HQ-73NSM.1gou_vqz1yw.api-client-public";
         return samlRequestReceiver(model, paramSamlRequest, paramRelayState);
     }
 
@@ -106,6 +105,39 @@ public class SamlController {
         log.debug("Model: [{}]", formData.toString());
 
         return "select_view";
+    }
+
+
+    /**
+     * Erzeugt einen Test-SAML-Request mit Beispielwerten und gibt ihn Base64-kodiert zurück.
+     * This is not a real SAML-Request. Just the bare skeleton for the following functions to work.
+     *
+     * @return Base64-kodierter SAML-AuthnRequest
+     */
+    private String createSkeletonSamlRequestBase64Encoded() {
+        String ascUrl = "http://localhost:8443/auth/realms/test/broker/bundid/endpoint";
+        String destUrl = "http://localhost:8080/saml";
+        String id = "test-request-id-12345";
+        String issuer = "http://localhost:8443/auth/realms/test";
+
+        String xml = String.format("""
+                <samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
+                    xmlns="urn:oasis:names:tc:SAML:2.0:assertion"
+                    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+                    AssertionConsumerServiceURL="%s"
+                    AttributeConsumingServiceIndex="0"
+                    Destination="%s"
+                    ID="%s"
+                    IssueInstant="2025-01-01T00:00:00.000Z"
+                    ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+                    Version="2.0">
+                  <saml:Issuer>%s</saml:Issuer>
+                  <samlp:RequestedAuthnContext Comparison="minimum">
+                    <saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Kerberos</saml:AuthnContextClassRef>
+                  </samlp:RequestedAuthnContext>
+                </samlp:AuthnRequest>""", ascUrl, destUrl, id, issuer);
+
+        return Base64.getEncoder().encodeToString(xml.getBytes(StandardCharsets.UTF_8));
     }
 
     // Helper **********************************************************************************************************
